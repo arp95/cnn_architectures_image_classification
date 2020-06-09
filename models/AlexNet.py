991320c6bf1fa77b7e42f8803174ca0251540bca
Arpit Aggarwal <arpitdec5@gmail.com>
2020-06-08 22:04:12 -0400

    Updated the AlexNet.py file in models folder

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -3,25 +3,31 @@ import torch
 import torch.nn as nn
 import torchvision
 import numpy as np
-import cv2
 
 
-# define alexnet architecture (remember input size: (224 x 224 x 3))
+# Define AlexNet CNN Architecture (remember input size: (224 x 224 x 3))
 class AlexNet(torch.nn.Module):
     
     # init function
     def __init__(self, num_classes = 2):
         super(AlexNet, self).__init__()
         
+        # first part - find conv-features
         self.features = torch.nn.Sequential(
+   
+            # first conv block
             torch.nn.Conv2d(3, 64, kernel_size=11, padding=2, stride=4),
             torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(inplace=True),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
+
+            # second conv block
             torch.nn.Conv2d(64, 192, kernel_size=5, padding=2),
             torch.nn.BatchNorm2d(192),
             torch.nn.ReLU(inplace=True),
             torch.nn.MaxPool2d(kernel_size=3, stride=2),
+
+            # third conv block
             torch.nn.Conv2d(192, 384, kernel_size=3, padding=1),
             torch.nn.BatchNorm2d(384),
             torch.nn.ReLU(inplace=True),
@@ -34,19 +40,28 @@ class AlexNet(torch.nn.Module):
             torch.nn.MaxPool2d(kernel_size=3, stride=2)
         )
         
+        # second part - apply adaptive average pool before flattening the conv-features into one vector
         self.avgpool = torch.nn.AdaptiveAvgPool2d((6, 6))
         
+        # third part - flatten the conv-features and apply a series of fc layers followed by the output layer
         self.classifier = torch.nn.Sequential(
+
+            # first fc layer
             torch.nn.Dropout(),
             torch.nn.Linear(256 * 6 * 6, 4096),
             torch.nn.ReLU(inplace=True),
+
+            # second fc layer
             torch.nn.Dropout(),
             torch.nn.Linear(4096, 4096),
             torch.nn.ReLU(inplace=True),
+
+            # third fc layer
             torch.nn.Linear(4096, num_classes),
             torch.nn.LogSoftmax(dim=1)
         )
         
+
     # forward propagation step
     def forward(self, x):
         x = self.features(x)
